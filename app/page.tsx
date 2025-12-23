975e41d7c41dc51728730f80ef69cc43bb3d1c73
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-12-23 10:37:07 +0200

    added the about section and the accent color

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "../components/HeroSection";
 import StatsBar from "@/components/StatsBar";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <div className="min-h-screen items-center justify-center bg-background relative font-lalezar">
       <HeroSection />
       <StatsBar  />
+      <About />
     </div>
   );
 }
---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function About() {
+  return (
+    <section className="w-full h-[80vh] flex flex-col items-center justify-center px-5 gap-5 text-center">
+      <h2 className="text-4xl">About me</h2>
+      <p className="text-xl w-11/12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <Link href="/about" className="px-5 py-3 bg-accent rounded-lg text-xl">Let's Work Together</Link>
+    </section>
+  )
+}
