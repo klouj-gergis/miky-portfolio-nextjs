995f609c21cb6a9fccfbe937e2a580d33bb2060b
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2026-01-03 23:23:27 +0200

    updated the hero and about layout

--- components/About.tsx
+++ components/About.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <section className="w-full h-[80vh] flex flex-col items-center justify-center px-5 gap-5 text-center">
+    <section className="w-full h-[80vh] flex flex-col justify-center px-5 gap-5">
       <h2 className="text-4xl">About me</h2>
       <p className="text-xl w-11/12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <Link href="/about" className="px-5 py-3 bg-accent rounded-lg text-xl">Let's Work Together</Link>
+      <Link href="/about" className="px-5 py-3 bg-accent rounded-lg text-xl w-fit">Let's Work Together</Link>
     </section>
   )
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="w-full h-screen flex flex-col items-center justify-center gap-10 overflow-hidden relative">
+    <section className="w-full h-screen flex flex-col items-center justify-center gap-10 overflow-hidden relative bg-black">
+      <div className="w-11/12 lg:w-full h-3/6 lg:h-full flex justify-center lg:justify-start lg:absolute lg:top-0 lg:left-0 border-3 lg:border-none rounded-2xl overflow-hidden ">
+        <Image src="/hero.nobg.png" alt="hero image" width={1300} height={800} className="h-full object-cover relative lg:absolute top-0 left-0" />
+      </div>
       <div className="lg:h-screen w-full lg:p-5 flex items-center">
         <h1 className="w-full text-7xl lg:text-[128px]  text-foreground flex flex-col lg:flex-row lg:justify-between items-center"><span>MGOMY</span><span>ماجومي</span></h1>
       </div>
-      <div className="w-11/12 lg:w-full h-3/6 lg:h-full flex justify-center lg:justify-start lg:absolute lg:top-0 lg:left-0 border-3 lg:border-none rounded-2xl overflow-hidden shadow-lg shadow-blue-500/50 lg:shadow-none">
-        <Image src="/hero.nobg.png" alt="hero image" width={1300} height={800} className="h-full object-cover relative lg:absolute top-0 left-0" />
-      </div>
+      
     </section>
   )
 }
